469469643cf8e7d61cba5b21b1ec15ef3304d158
V-Roman-V <vor.roman.v@gmail.com>
2021-10-06 01:29:58 +0300

    Implement first version of interface

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,47 @@
 public class Main {
+    private static void clear(){
+//        System.out.print("\033[H\033[2J");
+//        System.out.flush();
+//        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.print("\n~~~~~~~~~~~~~~~~~~~~~~\n");
+    }
     public static void main(String[] args) {
         Site site = new Site();
-        site.enterSite();
-
+        while (true) { // enter the site
+            clear();
+            site.enterSite();
+            int choose = site.startMenu();
+            if (choose == Site.INCORRECT) continue;
+            if (choose == Site.EXIT) break;
+            if (choose == Site.RETURN_BOOK) { // returning the book
+                clear();
+                site.provideReturnForm();
+                site.returnABook();
+                continue;
+            }
+            if (choose == Site.SEE_BOOKS)
+                while (true) { // Book previews
+                    clear();
+                    site.printBookList();
+                    int ID = site.chooseABook();
+                    if (ID == Site.EXIT) break;
+                    if (ID == Site.INCORRECT) continue;
+                    while (true) { // Viewing a particular book
+                        clear();
+                        site.printBookInfo(ID);
+                        int bk = site.askAboutBooking(ID);
+                        if (bk == Site.EXIT) break;
+                        if (bk == Site.NO) break;
+                        if (bk == Site.INCORRECT) continue;
+                        if (bk == Site.YES) {// Book reservation
+                            clear();
+                            site.provideReservationForm();
+                            site.bookABook(ID);
+                        }
+                    }
+                }
+        }
+        clear();
+        site.exitSite();
     }
 }
--- src/Site.java
+++ src/Site.java
@@ -4,42 +4,92 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Site {
-    private static final int INCORRECT = -2;
-    private static final int EXIT = -1;
+    public static final int INCORRECT = -2;
+    public static final int EXIT = -1;
+    public static final int RETURN_BOOK = 0;
+    public static final int SEE_BOOKS = 1;
+    public static final int YES = 1;
+    public static final int NO = 0;
 
     private final DataBase listOfBooks;
-
     public Site(){listOfBooks = DataBase.getInstance();}
-    public void enterSite(){}
-    public void exitSite(){}
-    public Book chooseABook(Integer bookID){return new Book();}
-    public void bookABook(Book book){}
-    public void returnABook(Integer bookID){}
+    public void enterSite(){System.out.println("Welcome to the \"Booka\" Bookstore website. \nWhat do you want to do?");}
+    public void exitSite(){System.out.println("We will be glad to meet you again in our bookstore  \"Booka\"");}
+
+    public int startMenu(){
+        String[] variants = {"return_book","see_books","exit"};
+        return AskUser(variants);
+    }
+
+    public int chooseABook(){
+        System.out.print("Choose bookID or ");
+        String[] exit = {"exit"};
+        printVariantsList(exit);
+
+        String str = getInput();
+        if(isExit(str)) return EXIT;
+        if(!isNumber(str)) return INCORRECT;
+        int id = Integer.parseInt(str);
+        if(!listOfBooks.checkBookID(id)) return INCORRECT;
+        return id;
+    }
+    public void printBookInfo(int bookID){
+
+    }
+    public int askAboutBooking(int bookID){
+        if(listOfBooks.getBook(bookID).isBooked()){
+            System.out.println("This book is already booked.");
+            String[] variants = {"exit"};
+            return AskUser(variants);
+        } else {
+            System.out.println("Want to reserve a book?");
+            String[] variants = {"no","yes","exit"};
+            return AskUser(variants);
+        }
+    }
+    public void bookABook(int bookID) {
+        System.out.print("Print your name or ");
+        String[] exit = {"exit"};
+        printVariantsList(exit);
+        String name = getInput();
+        if (isExit(name)) return;
+
+        System.out.println("To book a book, you will need to pay money, do you agree?");
+        String[] variants = {"no", "yes", "exit"};
+        if (AskUser(variants) == YES)
+            listOfBooks.getBook(bookID).setBooker(name);
+    }
+    public void provideReturnForm(){}
+    public void provideReservationForm(){}
+    public void returnABook(){}
 
 
-    private void printBookList(){
+    public void printBookList(){
         Book[] list = listOfBooks.getBookList();
     }
-    private void showBookInfo(){}
 
     private int AskUser(String @NotNull [] var){
-        Scanner sc=new Scanner(System.in);
-        printMenu(var);
-        String answer;
-        answer = sc.nextLine();
-        return calculateInput(answer, var);
+        printVariantsList(var);
+        return calculateInput(getInput(), var);
     }
 
-    private void printMenu(String @NotNull [] variants){
+    private boolean isExit(String str){
+        return (str.equals("-") || str.equals("exit"));
+    }
+    private String getInput(){
+        Scanner sc=new Scanner(System.in);
+        return sc.nextLine();
+    }
+    private void printVariantsList(String @NotNull [] variants){
         StringBuilder out = new StringBuilder("{");
         for(int i = 0; i<variants.length; i++)
             out.append("(").append(variants[i].equals("exit") ? "-" : Integer.toString(i)).append(")").append(variants[i]).append((i + 1 == variants.length) ? "}" : "; ");
         System.out.println(out);
     }
     private int calculateInput(@NotNull String str, String[] variants){
-        if(str.equals("-") || str.equals("exit")) return EXIT;
+        if(isExit(str)) return EXIT;
         if((isNumber(str))){
-            int hasExit = (Arrays.stream(variants).anyMatch("exit"::equals))? 1 : 0;
+            int hasExit = (Arrays.asList(variants).contains("exit"))? 1 : 0;
             int i = Integer.parseInt(str);
             if( i< 0 || i >= variants.length-hasExit) return INCORRECT;
             return i;
