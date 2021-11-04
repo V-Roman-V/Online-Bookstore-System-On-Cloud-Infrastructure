77a3ca7db66740e9be5ca47bdbb0b64fc8421f84
Artem Voronov <voronov_artem_lit@mail.ru>
2021-11-04 15:44:30 +0300

    feat: Add reader entity

---
+++ src/implementation/database/entity/Reader.java
@@ -0,0 +1,41 @@
+package implementation.database.entity;
+
+import abstraction.database.entity.EntityInterface;
+
+public class Reader implements EntityInterface {
+
+    /**
+     * Coefficient which makes id unique for each readers
+     */
+    private static int ID_coefficient;
+
+    /**
+     * fields of readerTable
+     */
+    public Integer reader_ticket;
+    public String first_name;
+    public String last_name;
+    public String address;
+    public String phone_number;
+
+    public Reader(String first_name, String last_name) {
+        reader_ticket = ++ID_coefficient;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public Reader phoneNumber(String phone_number) {
+        this.phone_number = phone_number;
+        return this;
+    }
+
+    public Reader address(String address) {
+        this.address = address;
+        return this;
+    }
+
+    @Override
+    public String getKey() {
+        return first_name + last_name;
+    }
+}
