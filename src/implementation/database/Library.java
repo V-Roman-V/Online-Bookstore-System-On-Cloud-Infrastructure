8c884cccd3c706dc2574171f7c99de6c9864ccb4
V-Roman-V <vor.roman.v@gmail.com>
2021-11-04 20:41:33 +0300

    Implement requests

--- src/implementation/database/Library.java
+++ src/implementation/database/Library.java
@@ -1,73 +1,77 @@
 package implementation.database;
 
-import implementation.database.entity.Author;
-import implementation.database.entity.Book;
-import implementation.database.entity.Genre;
+import implementation.database.entity.*;
 
 import abstraction.database.*;
-import implementation.database.entity.Reader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
- * A class representing Database which stores books
+ * A class representing Library which stores books
  */
 public final class Library implements DataBaseInterface{
-    /**
-     * Singleton object
-     */
+
+    /** Singleton object */
     private static Library instance;
 
-    /**
-     * A Hashmap which stores a databases
-     */
+    /** Hashmaps which stores a databases */
     final HashMap<String, Book> book_table;
     final HashMap<String, Author> author_table;
     final HashMap<String, Reader> reader_table;
     final HashMap<String, Genre> genre_table;
-    final HashMap<String, Order> order_table;
+    final HashMap<String, Order> current_order_table;
+    final HashMap<String, Order> archived_order_table;
 
-    /**
-     * Creates a database using the genres hashmap
-     *
-     * @param genres hashmap storing an array of book by genre
-     */
+    /** Creates a library */
     private Library() {
         book_table   = new HashMap<String, Book>();
         author_table = new HashMap<String, Author>();
         reader_table = new HashMap<String, Reader>();
         genre_table  = new HashMap<String, Genre>();
-        order_table  = new HashMap<String, Order>();
+        current_order_table  = new HashMap<String, Order>();
+        archived_order_table = new HashMap<String, Order>();
     }
 
     /**
-     * @param genres hashmap An alternative to the constructor and is the access
-     *               point to an instance of Database class.
+     * An alternative to the constructor and
+     * is the access point to an instance of Library class.
      */
-    public static Library getInstance(HashMap<Genre, ArrayList<Book>> genres) {
+    public static Library getInstance() {
         if (instance == null)
-            instance = new Library(genres);
+            instance = new Library();
         return instance;
     }
 
-    /**
-     * @return genre_of_books hashmap
-     */
-    public HashMap<Genre, ArrayList<Book>> getBooksGenre() {
-        return genre_of_books;
+    @Override
+    public ArrayList<Book> getBooksByAuthor(Author author){
+        return new ArrayList<>(book_table.values().stream().filter( book -> book.author == author).toList());
     }
 
-    /**
-     * @param BookID
-     * @return book stored in genre_of_books hashmap and which BookID converges with
-     *         the parameter
-     */
-    public Book getBook(int BookID) {
-        for (ArrayList<Book> v : genre_of_books.values())
-            for (Book book : v)
-                if (book.getID() == BookID)
-                    return book;
-        return null;
+    @Override
+    public ArrayList<Book> getBooksByGenre(Genre genre){
+        return new ArrayList<>(book_table.values().stream().filter( book -> book.genre == genre).toList());
+    }
+
+    @Override
+    public ArrayList<Genre> getListOfGenres(){
+        return new ArrayList<>(genre_table.values());
+    }
+
+    @Override
+    public ArrayList<Author> getListOfAuthors(){
+        return new ArrayList<>(author_table.values());
+    }
+
+    @Override
+    public ArrayList<Book> getListOfBooks(){
+        return new ArrayList<>(book_table.values());
+    }
+
+    @Override
+    public Boolean getIsBookAvailable(Book book){
+        return current_order_table.values().stream().anyMatch( order -> order.book == book);
     }
 }
