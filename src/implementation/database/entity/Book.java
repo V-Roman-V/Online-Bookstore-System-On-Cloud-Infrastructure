040537baa7f0668b53a3b91b74c3b4057ab0eaff
V-Roman-V <vor.roman.v@gmail.com>
2021-11-04 15:14:26 +0300

    fix errors with git

---
+++ src/implementation/database/entity/Book.java
@@ -0,0 +1,103 @@
+package implementation.database.entity;
+
+import implementation.database.entity.Genre;
+import pureooabstraction.database.EntityInterface;
+
+/**
+ * A class representing Implementation.database.object.Book
+ */
+public class Book implements EntityInterface {
+    /**
+     * Coefficient which makes id unique for each book
+     */
+    private static int ID_coefficient;
+
+    /**
+     * Properties of books
+     */
+    private final String title;
+    private final String notes;
+    private final String author;
+    private final Genre genre;
+    private final double price;
+    private final int ID;
+    private boolean booked;
+    private String bookerName;
+
+    /**
+     * Creates a book
+     */
+    public Book(String title, Genre genre, String author, double price) {
+        ID = ++ID_coefficient;
+        this.title = title;
+        this.genre = genre;
+        this.author = author;
+        this.price = price;
+        this.notes = "";
+    }
+
+    /**
+     * @return was the book actually booked
+     */
+    public boolean isBooked() {
+        return booked;
+    }
+
+    /**
+     * Makes book free
+     */
+    public void releaseBook() {
+        this.booked = false;
+    }
+
+    /**
+     * @param bookerName Makes book booked and set bookerName which converges with
+     *                   parameter
+     */
+    public void setBooker(String bookerName) {
+        this.bookerName = bookerName;
+        this.booked = true;
+    }
+
+    /**
+     * @return the title of book
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @return id of the book
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @return author of the book
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @return genre of the book
+     */
+    public Genre getGenre() {
+        return genre;
+    }
+
+    /**
+     * @return name of the booker
+     */
+    public String getBookerName() {
+        return bookerName;
+    }
+
+    /**
+     * @return price of the book
+     */
+    public double getPrice() {
+        return price;
+    }
+}
